4f44459ca31891b107d855c0f6e42ec45e44b2b0
Roberto <robertopadillapadilla9@gmail.com>
2025-03-28 16:44:57 -0500

    Migracion de aforo cambiada a varchar

--- database/migrations/2025_03_28_162215_update_aforo_nullable_in_data_table.php
+++ database/migrations/2025_03_28_162215_update_aforo_nullable_in_data_table.php
@@ -8,22 +8,19 @@ use Illuminate\Support\Facades\Schema;
 return new class extends Migration {
     public function up(): void
     {
-        // Convertir aforo a VARCHAR temporalmente para evitar errores de conversión
-        DB::statement('ALTER TABLE data MODIFY aforo VARCHAR(255)');
-
-        // Reemplazar valores inválidos por NULL
-        DB::statement("UPDATE data SET aforo = NULL WHERE aforo NOT REGEXP '^[0-9]+$'");
-
-        // Convertir aforo de nuevo a INTEGER y permitir valores NULL
+        // Convertir aforo a VARCHAR(255)
         Schema::table('data', function (Blueprint $table) {
-            $table->integer('aforo')->nullable()->change();
+            $table->string('aforo', 255)->nullable()->change();
         });
     }
 
     public function down(): void
     {
+        // Reemplazar valores no numéricos por NULL antes de convertir a INTEGER
+        DB::statement("UPDATE data SET aforo = NULL WHERE aforo NOT REGEXP '^[0-9]+$'");
+
         Schema::table('data', function (Blueprint $table) {
-            $table->integer('aforo')->nullable(false)->change();
+            $table->integer('aforo')->nullable()->change();
         });
     }
 };
