f9119db57e06178372a35bc273199f4f6cd10a20
Roberto <robertopadillapadilla9@gmail.com>
2025-03-28 16:24:53 -0500

    f

--- database/migrations/2025_03_28_162215_update_aforo_nullable_in_data_table.php
+++ database/migrations/2025_03_28_162215_update_aforo_nullable_in_data_table.php
@@ -2,22 +2,24 @@
 
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Schema;
 
 return new class extends Migration {
-    /**
-     * Run the migrations.
-     */
     public function up(): void
     {
+        // Convertir aforo a VARCHAR temporalmente para evitar errores de conversión
+        DB::statement('ALTER TABLE data MODIFY aforo VARCHAR(255)');
+
+        // Reemplazar valores inválidos por NULL
+        DB::statement("UPDATE data SET aforo = NULL WHERE aforo NOT REGEXP '^[0-9]+$'");
+
+        // Convertir aforo de nuevo a INTEGER y permitir valores NULL
         Schema::table('data', function (Blueprint $table) {
             $table->integer('aforo')->nullable()->change();
         });
     }
 
-    /**
-     * Reverse the migrations.
-     */
     public function down(): void
     {
         Schema::table('data', function (Blueprint $table) {
